d0e96f51389fdfd9a7a8afd532394208d515418e
Jelly Bool <ly913417651@gmail.com>
2015-01-28 23:51:36 +0800

    fix database.php
    change laravel-start to laravel

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'laravel-start',
+			'database'  => 'laravel',
 			'username'  => 'root',
-			'password'  => 'var_dump(mysql)',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
